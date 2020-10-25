ea257ceceb3d6d83653f5ab8a5a5cef64a64ae36
Giuliano Gabella <ggabella91@gmail.com>
2020-10-25 11:03:04 -0700

    Added tests for auth-photo route handlers, wired up route handlers to the express app.

--- auth-photo/src/app.ts
+++ auth-photo/src/app.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@ggabella-photo-share/common';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -13,6 +18,11 @@ app.use(
     secure: false,
   })
 );
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
