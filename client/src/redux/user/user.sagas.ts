f953d14b6bfd8a3ef97f2e301b2a5173bf565c0a
Giuliano Gabella <ggabella91@gmail.com>
2020-10-29 22:06:54 -0700

    Started implementing user sagas.

--- client/src/redux/user/user.sagas.ts
+++ client/src/redux/user/user.sagas.ts
@@ -1,9 +1,64 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
+import { SagaIterator } from '@redux-saga/core';
+import {ActionPattern} from '@redux-saga/types'
 
-import { UserActionTypes } from './user.types';
+import { UserSignUp, User, UserActions, UserActionTypes } from './user.types';
 
 import axios from 'axios';
 
-export function* userSagas() {
+import {
+  setCurrentUser,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  signUpSuccess,
+  signUpFailure,
+} from './user.actions';
+
+export function* signUp({
+  payload: { name, email, password, passwordConfirm },
+}: {
+  UserActionTypes;
+}): SagaIterator {
+  try {
+    const { data } = yield axios.post('api/v1/users/signup', {
+      name,
+      email,
+      password,
+      passwordConfirm,
+    });
+
+    yield put(signUpSuccess(data.data.user));
+  } catch (err) {
+    yield put(signUpFailure(err));
+  }
+}
+
+export function* onSignUpStart(): SagaIterator {
+  yield takeLatest<ActionPattern>(UserActions.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess(): SagaIterator {
+  yield takeLatest(UserActions.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
+export function* signInAfterSignUp({ payload: user }): SagaIterator {
+  yield put(setCurrentUser(user));
+}
+
+export function* onSignInStart(): SagaIterator {
+  yield takeLatest(UserActions.SIGN_IN_START, signIn);
+}
+
+export function* onCheckUserSession(): SagaIterator {
+  yield takeLatest(UserActions.CHECK_USER_SESSION, isLoggedIn);
+}
+
+export function* onSignOutStart(): SagaIterator {
+  yield takeLatest(UserActions.SIGN_OUT_START, signOut);
+}
+
+export function* userSagas(): SagaIterator {
   yield all([]);
 }
--- client/src/redux/user/user.types.ts
+++ client/src/redux/user/user.types.ts
@@ -1,16 +1,16 @@
-export const UserActions = {
-  CHECK_USER_SESSION: 'CHECK_USER_SESSION',
-  SET_CURRENT_USER: 'SET_CURRENT_USER',
-  SIGN_UP_START: 'SIGN_UP_START',
-  SIGN_UP_SUCCESS: 'SIGN_UP_SUCCESS',
-  SIGN_UP_FAILURE: 'SIGN_UP_FAILURE',
-  SIGN_IN_START: 'SIGN_IN_START',
-  SIGN_IN_SUCCESS: 'SIGN_IN_SUCCESS',
-  SIGN_IN_FAILURE: 'SIGN_IN_FAILURE',
-  SIGN_OUT_START: 'SIGN_OUT_START',
-  SIGN_OUT_SUCCESS: 'SIGN_OUT_SUCCESS',
-  SIGN_OUT_FAILURE: 'SIGN_OUT_FAILURE',
-};
+export enum UserActions {
+  CHECK_USER_SESSION = 'CHECK_USER_SESSION',
+  SET_CURRENT_USER = 'SET_CURRENT_USER',
+  SIGN_UP_START = 'SIGN_UP_START',
+  SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS',
+  SIGN_UP_FAILURE = 'SIGN_UP_FAILURE',
+  SIGN_IN_START = 'SIGN_IN_START',
+  SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS',
+  SIGN_IN_FAILURE = 'SIGN_IN_FAILURE',
+  SIGN_OUT_START = 'SIGN_OUT_START',
+  SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS',
+  SIGN_OUT_FAILURE = 'SIGN_OUT_FAILURE',
+}
 
 export interface UserState {
   currentUser: { name: string; email: string } | null;
@@ -98,12 +98,13 @@ export interface SignOutFailure {
 export type UserActionTypes =
   | SignUpStart
   | SignUpSuccess
-  | SignOutFailure
+  | SignUpFailure
   | SignInStart
   | SignInSuccess
   | SignInFailure
   | SignOutStart
   | SignOutSuccess
+  | SignOutFailure
   | SignOutFailure
   | CheckUserSession
   | SetCurrentUser;
