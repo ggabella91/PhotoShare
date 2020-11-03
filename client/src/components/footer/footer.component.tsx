9428a23739458fb8c9e3c0444099144e89b5929a
Giuliano Gabella <ggabella91@gmail.com>
2020-11-02 18:33:26 -0800

    Modifications to form-input styling. Added a footer component.

---
+++ client/src/components/footer/footer.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer: React.FC = () => (
+  <div>
+    <div className='footer'>
+      <span>Created by Giuliano Gabella</span>
+    </div>
+  </div>
+);
+
+export default Footer;
