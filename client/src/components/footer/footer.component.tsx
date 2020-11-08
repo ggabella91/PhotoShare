330c080ae8cdcd7729eddd1138d26849c738819a
Giuliano Gabella <ggabella91@gmail.com>
2020-11-08 14:16:38 -0800

    Added photo-gallery favicon.

--- client/src/components/footer/footer.component.tsx
+++ client/src/components/footer/footer.component.tsx
@@ -6,6 +6,12 @@ const Footer: React.FC = () => (
   <div>
     <div className='footer'>
       <span>Created by Giuliano Gabella</span>
+      <a
+        className='favicon-link'
+        href='https://icons8.com/icon/Z6RmPnIUvybQ/photo-gallery'
+      >
+        Photo Gallery icon by Icons8
+      </a>
     </div>
   </div>
 );
