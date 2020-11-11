130df2e5bd93054441f6bb7f0a97b390ed2cc049
Giuliano Gabella <ggabella91@gmail.com>
2020-11-10 21:46:15 -0800

    Started implementing forgot-password route handler in auth service.

---
+++ auth/src/routes/forgot-password.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User, userSchema } from '../models/user';
+import {
+  validateRequest,
+  BadRequestError,
+  NotFoundError,
+  InternalServerError,
+} from '@ggabella-photo-share/common';
+
+const router = express.Router();
+
+router.post(
+  '/api/users/forgotPassword',
+  [body('email').isEmail().withMessage('Email must be valid')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      throw new NotFoundError();
+    }
+
+    const resetToken = user.schema.methods.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    try {
+      const resetURL = `${req.protocol}://photo-share.dev/reset-password/${resetToken}`;
+
+      // SEND EMAIL TO USER - NEED TO IMPLEMENT EMAIL SERVICE
+
+      res
+        .status(200)
+        .send({ status: 'success', message: 'Token sent to email!' });
+    } catch (err) {
+      user.passwordResetToken = undefined;
+      user.passwordResetExpires = undefined;
+      await user.save({ validateBeforeSave: false });
+
+      throw new InternalServerError();
+    }
+  }
+);
