4a04d7c6c34de90930143a0d41c564a40dfa3216
Giuliano Gabella <ggabella91@gmail.com>
2020-11-14 22:45:47 -0800

    Added modal component to client.

---
+++ client/src/components/modal/modal.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from '../button/button.component';
+
+interface Props {
+  header: string;
+  subheader: string;
+  bodytext: string;
+  onHide: () => {};
+  handleConfirm: () => {};
+  actionLabel: string;
+}
+
+const CustomModal: React.FC<Props> = (props) => (
+  <Modal {...props} dialogClassName='custom-modal' centered>
+    <Modal.Header closeButton>
+      <Modal.Title id='contained-modal-title-vcenter'>
+        {props.header}
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body className='body-text'>
+      <h4>{props.subheader}</h4>
+      <p>{props.bodytext}</p>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button className='button modal-button' onSubmit={props.onHide}>
+        Cancel
+      </Button>
+      <Button
+        className='button modal-button delete-button'
+        onSubmit={props.handleConfirm}
+      >
+        {props.actionLabel}
+      </Button>
+    </Modal.Footer>
+  </Modal>
+);
+
+export default CustomModal;
