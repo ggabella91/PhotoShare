ac760a42a1e19de99ac3b9a85984f5c7a79e2274
Giuliano Gabella <ggabella91@gmail.com>
2020-11-18 17:51:17 -0800

    Updated server files in each service to remove try-catch block, to allow deployments to restart if the services are not able to connect to NATS on any particular startup attempt.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -23,28 +23,24 @@ const start = async () => {
     throw new Error('CLUSTER_ID must be defined');
   }
 
-  try {
-    await natsWrapper.connect(
-      process.env.NATS_CLUSTER_ID,
-      process.env.NATS_CLIENT_ID,
-      process.env.NATS_URL
-    );
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!');
-      process.exit();
-    });
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
+  await natsWrapper.connect(
+    process.env.NATS_CLUSTER_ID,
+    process.env.NATS_CLIENT_ID,
+    process.env.NATS_URL
+  );
+  natsWrapper.client.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+  process.on('SIGINT', () => natsWrapper.client.close());
+  process.on('SIGTERM', () => natsWrapper.client.close());
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (err) {
-    console.log(err);
-  }
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+  console.log('Connected to MongoDB');
 
   app.listen(3000, () => {
     console.log('Listening on port 3000!!!!');
