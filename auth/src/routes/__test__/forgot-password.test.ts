96b82f08ff522579fddaabc4ce8ffb33c79106a1
Giuliano Gabella <ggabella91@gmail.com>
2020-11-26 12:16:21 -0800

    Started writing tests for forgot-password route handler.

---
+++ auth/src/routes/__test__/forgot-password.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('Creates a password reset token', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      name: 'Test Dude',
+      email: 'test@test.com',
+      password: 'password',
+      passwordConfirm: 'password',
+    })
+    .expect(201);
+
+  const { body: user } = await request(app)
+    .post('/api/users/forgot-password')
+    .send({
+      email: 'test@test.com',
+    });
+
+  expect(user.passwordResetExpires).toBeDefined();
+});
