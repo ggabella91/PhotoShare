ced4dcc4ad4ca3a676003623880c5b16c6cd8069
Giuliano Gabella <ggabella91@gmail.com>
2020-12-08 19:17:00 -0800

    Created my-profile page component and connected it to the main app component.

---
+++ client/src/pages/my-profile/my-profile-page.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './my-profile-page.styles.scss';
+
+const MyProfilePage: React.FC = () => {
+  return <div className='my-profile-page'>My Profile Page</div>;
+};
+
+export default MyProfilePage;
