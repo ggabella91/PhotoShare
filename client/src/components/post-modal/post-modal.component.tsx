9564c8b7dd1ce231131b4d1ae520b6bc44e20f19
Giuliano Gabella <ggabella91@gmail.com>
2020-12-21 21:31:32 -0800

    Further updates to post-modal updates and styling, using two different post image elements that display conditionally

--- client/src/components/post-modal/post-modal.component.tsx
+++ client/src/components/post-modal/post-modal.component.tsx
@@ -30,47 +30,43 @@ const PostModal: React.FC<Props> = ({
 
   return (
     <Modal {...props} dialogClassName='post-modal' animation={false} centered>
-      <div className='column-alignment'>
-        <div className='large-image-header-body'>
-          <img
-            className='post-modal-image-large'
-            src={`data:image/jpeg;base64,${fileString}`}
-            alt='user'
-          />
-          <div>
-            <Modal.Header className='post-modal-header' closeButton />
-            <Modal.Body className='post-modal-body'>
-              <img
-                className='post-modal-image-embedded'
-                src={`data:image/jpeg;base64,${fileString}`}
-                alt='post-pic'
-              />
-              <div className='post-modal-details'>
-                <div className='post-user-and-location'>
-                  <img
-                    className='user-photo'
-                    src={`data:image/jpeg;base64,${userProfilePhotoFile}`}
-                    alt='user'
-                  />
-                  <div className='text-and-options'>
-                    <div className='user-and-location'>
-                      <span className='user-name'>{userName}</span>
-                      <span className='post-location'>{location}</span>
-                    </div>
-                    <div className='post-options'>
-                      <span className='ellipsis' onClick={onOptionsClick}>
-                        ...
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <span className='post-caption'>{caption}</span>
-                <span className='post-date'>{postDate}</span>
-              </div>
-            </Modal.Body>
-          </div>
-        </div>
+      <div className='large-image-adjustments'>
+        <img
+          className='post-modal-image-large'
+          src={`data:image/jpeg;base64,${fileString}`}
+          alt='user'
+        />
       </div>
+      <Modal.Header className='post-modal-header' closeButton />
+      <Modal.Body className='post-modal-body'>
+        <img
+          className='post-modal-image-embedded'
+          src={`data:image/jpeg;base64,${fileString}`}
+          alt='post-pic'
+        />
+        <div className='post-modal-details'>
+          <div className='post-user-and-location'>
+            <img
+              className='user-photo'
+              src={`data:image/jpeg;base64,${userProfilePhotoFile}`}
+              alt='user'
+            />
+            <div className='text-and-options'>
+              <div className='user-and-location'>
+                <span className='user-name'>{userName}</span>
+                <span className='post-location'>{location}</span>
+              </div>
+              <div className='post-options'>
+                <span className='ellipsis' onClick={onOptionsClick}>
+                  ...
+                </span>
+              </div>
+            </div>
+          </div>
+          <span className='post-caption'>{caption}</span>
+          <span className='post-date'>{postDate}</span>
+        </div>
+      </Modal.Body>
     </Modal>
   );
 };
