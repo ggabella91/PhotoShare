b46a1e256045754a240fbf831afeaf690bc39e9a
Giuliano Gabella <ggabella91@gmail.com>
2020-12-24 12:30:49 -0800

    Updated component tests, made header component exported for testing, updated post-modal component styles

--- client/src/components/__test__/header.test.tsx
+++ client/src/components/__test__/header.test.tsx
@@ -1,9 +1,22 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import Header from '../header/header.component';
+import { Header } from '../header/header.component';
+import { PostFileReq } from '../../redux/post/post.types';
+import { getPostFileStart } from '../../redux/post/post.actions';
+import { signOutStart } from '../../redux/user/user.actions';
 
 it('renders a header component', () => {
-  const headerWrapper = shallow(<Header />);
+  // const fileReq: PostFileReq = { s3Key: 'key', bucket: 'bucket' };
+
+  const headerWrapper = shallow(
+    <Header
+      currentUser={null}
+      profilePhotoFile='photo'
+      profilePhotoKey='photo-key'
+      getPostFileStart={(fileReq) => getPostFileStart(fileReq)}
+      signOutStart={() => signOutStart()}
+    />
+  );
 
   expect(headerWrapper).toMatchSnapshot();
 });
