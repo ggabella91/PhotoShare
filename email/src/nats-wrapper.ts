b4ae0aebbe3864abb01599bc37e7c1b2b73003aa
Giuliano Gabella <ggabella91@gmail.com>
2020-12-28 17:46:24 -0800

    Fix to nats-wrapper in each service after updates to typescript

--- email/src/nats-wrapper.ts
+++ email/src/nats-wrapper.ts
@@ -17,7 +17,7 @@ class NatsWrapper {
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
         console.log('Connected to NATS');
-        resolve();
+        resolve('');
       });
 
       this.client.on('error', (err) => {
