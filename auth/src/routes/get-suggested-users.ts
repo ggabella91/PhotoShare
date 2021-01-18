28ea13ff8e13078e5e94587126a49f08ba178fff
Giuliano Gabella <ggabella91@gmail.com>
2021-01-18 13:08:43 -0800

    Small fixes to the get-suggested users route handler in auth service

--- auth/src/routes/get-suggested-users.ts
+++ auth/src/routes/get-suggested-users.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { User } from '../models/user';
+import { User, UserDoc } from '../models/user';
 import { BadRequestError } from '@ggabella-photo-share/common';
 
 const router = express.Router();
@@ -17,10 +17,10 @@ router.get(
     }).limit(10);
 
     if (!existingUser) {
-      throw new BadRequestError('No username matches found');
+      throw new BadRequestError('No username or name matches found');
     }
 
-    res.status(200).send(existingUser.map((el) => el.toJSON()));
+    res.status(200).send(existingUser.map((el: UserDoc) => el.toJSON()));
   }
 );
 
