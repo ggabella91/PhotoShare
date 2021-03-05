9b7a1ed56711ad7a4650dd944d6338d2b2cf2223
Giuliano Gabella <ggabella91@gmail.com>
2021-03-04 18:25:47 -0800

    Added feed-postt-container component and styles that will go into feed-page component in client

---
+++ client/src/components/feed-post-container/feed-post-container.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './feed-post-container.styles.scss';
+
+interface FeedPostContainerProps {}
+
+const FeedPostContainer: React.FC<FeedPostContainerProps> = () => {
+  return (
+    <div className='feed-post-container'>
+      <div></div>
+    </div>
+  );
+};
+
+export default FeedPostContainer;
