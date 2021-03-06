b11192e5fb5bc0fe15ab34c05fee271f57967ec5
Giuliano Gabella <ggabella91@gmail.com>
2021-03-06 15:01:52 -0800

    Client: Refactored user-info component to make it even more reusable, modified class names in its stylesheet. Updated search-bar component. Added more tsx to the feed-post-container component

--- client/src/components/feed-post-container/feed-post-container.component.tsx
+++ client/src/components/feed-post-container/feed-post-container.component.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
-import './feed-post-container.styles.scss';
+import UserInfo, { StyleType } from '../user-info/user-info.component';
 
-interface FeedPostContainerProps {}
+import './feed-post-container.styles.scss';
+import { userInfo } from 'os';
+
+interface FeedPostContainerProps {
+  userInfo: UserInfoData;
+}
+
+export interface UserInfoData {
+  profilePhotoFileString: string;
+  username: string;
+  name: string;
+  photo: string;
+}
 
 const FeedPostContainer: React.FC<FeedPostContainerProps> = () => {
   return (
     <div className='feed-post-container'>
-      <div className='profile-and-options'></div>
+      <div className='profile-and-options'>
+        <UserInfo styleType={StyleType.feed} userInfoArray={[]} />
+      </div>
       <div className='post-photo'></div>
       <div className='likes-and-comments'></div>
     </div>
