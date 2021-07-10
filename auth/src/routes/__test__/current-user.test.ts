b232f4522387b2939a66b6a924dceea961d85258
Giuliano Gabella <ggabella91@gmail.com>
2021-07-10 15:36:05 -0700

    Auth: Fixed stupid typescript errors

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { global } from '../../test/setup';
 
 it('responds with details about the current user', async () => {
   const cookie = await global.signin();
