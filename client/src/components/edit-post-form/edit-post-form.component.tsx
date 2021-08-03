45f1ac16a650ae33320dd82002b80eb16d97e369
Giuliano Gabella <ggabella91@gmail.com>
2021-08-03 00:55:20 -0700

    Client: Added some state and effect hooks to edit-post-form component, wired it up to redux with an initial action dispatcher. Added some styling to the component

--- client/src/components/edit-post-form/edit-post-form.component.tsx
+++ client/src/components/edit-post-form/edit-post-form.component.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import { AppState } from '../../redux/root-reducer';
+
+import { setShowPostEditForm } from '../../redux/post/post.actions';
 
 import './edit-post-form.styles.scss';
 
 interface EditPostFormProps {
   editCaption: string;
   editLocation: string;
+  setShowPostEditForm: typeof setShowPostEditForm;
 }
 
 const EditPostForm: React.FC<EditPostFormProps> = ({
   editCaption,
   editLocation,
+  setShowPostEditForm,
 }) => {
+  const [editPostDetails, setEditPostDetails] = useState({
+    caption: '',
+    location: '',
+  });
+
+  useEffect(() => {
+    if (editCaption && editLocation) {
+      setEditPostDetails({ caption: editCaption, location: editLocation });
+    }
+  });
+
+  const { caption, location } = editPostDetails;
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = event.target;
+
+    setEditPostDetails({ ...editPostDetails, [name]: value });
+  };
+
   return (
     <form
       className='edit-post-form'
@@ -19,16 +47,20 @@ const EditPostForm: React.FC<EditPostFormProps> = ({
       }}
     >
       <input
+        className='input'
         type='text'
-        name='editCaption'
-        value={editCaption}
+        name='caption'
+        value={caption}
         placeholder='Add a caption'
+        onChange={handleInputChange}
       />
       <input
+        className='input'
         type='text'
-        name='editLocation'
-        value={editLocation}
+        name='location'
+        value={location}
         placeholder='Where was this taken?'
+        onChange={handleInputChange}
       />
       <div className='buttons-container'>
         <button
@@ -36,16 +68,26 @@ const EditPostForm: React.FC<EditPostFormProps> = ({
           onClick={() => {
             /*Add action creator and other redux for submitting edit-post request*/
           }}
-        />
+        >
+          Done Editing
+        </button>
         <button
           className='cancel-edit-button'
-          onClick={() => {
-            /*Set show edit-post form to false */
-          }}
-        />
+          onClick={() => setShowPostEditForm(false)}
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
 };
 
-export default EditPostForm;
+interface LinkStateProps {}
+
+const mapStateToProps = createStructuredSelector<AppState, LinkStateProps>({});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  setShowPostEditForm: (showPostEditForm: boolean) =>
+    dispatch(setShowPostEditForm(showPostEditForm)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(EditPostForm);
