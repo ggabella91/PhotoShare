5422b64f701885a071cedafdd2399c49dbdb8f66
Giuliano Gabella <ggabella91@gmail.com>
2021-11-27 19:48:43 -0800

    Client: Created test suite for button with second test, updated data-testid attribute in button component for testing

--- client/src/components/__test__/button.test.tsx
+++ client/src/components/__test__/button.test.tsx
@@ -1,10 +1,32 @@
-import { render } from '../../test-utils/test-utils';
+import { render, screen, fireEvent } from '../../test-utils/test-utils';
 import Button from '../button/button.component';
 
-it('renders a button component', () => {
-  const { container: button } = render(
-    <Button className='button-class' onClick={() => {}} />
-  );
+describe('button component tests', () => {
+  const setup = () => {
+    const clickHandler = jest.fn();
 
-  expect(button).toBeInTheDocument();
+    const buttonRenderResult = render(
+      <Button className='button-class' onClick={clickHandler} />
+    );
+
+    return { buttonRenderResult, clickHandler };
+  };
+
+  it('renders a button component', () => {
+    const {
+      buttonRenderResult: { container: button },
+    } = setup();
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('clicking button calls click handler', () => {
+    const { clickHandler } = setup();
+
+    const button = screen.getByTestId('button');
+
+    fireEvent.click(button);
+
+    expect(clickHandler).toBeCalled();
+  });
 });
