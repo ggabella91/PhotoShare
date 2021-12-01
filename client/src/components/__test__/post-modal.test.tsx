75054331cceb1aa2250bb7eb236a4138709ac25d
Giuliano Gabella <ggabella91@gmail.com>
2021-11-30 18:54:49 -0800

    Client: Refactored post-modal component test suite with a setup function, started refactoring first test

--- client/src/components/__test__/post-modal.test.tsx
+++ client/src/components/__test__/post-modal.test.tsx
@@ -1,66 +1,82 @@
-import { render } from '../../test-utils/test-utils';
+import { render, screen } from '../../test-utils/test-utils';
 import { PostModal } from '../post-modal/post-modal.component';
 
-import { getOtherUserStart } from '../../redux/user/user.actions';
+describe('post modal component tests', () => {
+  const setup = () => {
+    const handleHide = jest.fn();
+    const handleOptionsClick = jest.fn();
+    const createPostReactionStart = jest.fn();
+    const getPostReactionsStart = jest.fn();
+    const getPostFileStart = jest.fn();
+    const deleteReactionStart = jest.fn();
+    const getOtherUserStart = jest.fn();
+    const setPostLikingUsersArray = jest.fn();
+    const clearPostReactions = jest.fn();
+    const setShowPostEditForm = jest.fn();
+    const clearPostState = jest.fn();
+    const getSinglePostDataStart = jest.fn();
 
-import {
-  createPostReactionStart,
-  getPostReactionsStart,
-  getPostFileStart,
-  deleteReactionStart,
-  setPostLikingUsersArray,
-  clearPostReactions,
-  setShowPostEditForm,
-  getSinglePostDataStart,
-  clearPostState,
-} from '../../redux/post/post.actions';
+    render(
+      <PostModal
+        caption='hi'
+        location='here'
+        createdAt={new Date('Sat Jan 02 2021')}
+        show={true}
+        fileString='string'
+        userName='Giuliano'
+        onHide={handleHide}
+        onOptionsClick={handleOptionsClick}
+        userProfilePhotoFile='userFile'
+        postId=''
+        userId=''
+        createPostReactionStart={createPostReactionStart}
+        postReactionsArray={[]}
+        postReactionConfirm={null}
+        postReactionError={null}
+        getPostReactionsStart={getPostReactionsStart}
+        getPostReactionsConfirm=''
+        getPostReactionsError={null}
+        currentUser={null}
+        clearLocalState={false}
+        postReactingUsers={[]}
+        reactorPhotoFileArray={[]}
+        usersProfilePhotoConfirm={null}
+        deleteReactionConfirm={null}
+        deleteReactionError={null}
+        showPostEditForm={false}
+        editPostDetailsConfirm={null}
+        getPostFileStart={getPostFileStart}
+        getOtherUserStart={getOtherUserStart}
+        deleteReactionStart={deleteReactionStart}
+        setPostLikingUsersArray={setPostLikingUsersArray}
+        clearPostReactions={clearPostReactions}
+        setShowPostEditForm={setShowPostEditForm}
+        getSinglePostDataStart={getSinglePostDataStart}
+        clearPostState={clearPostState}
+      />
+    );
 
-it('expect to render a post modal component', () => {
-  const { container: postModal } = render(
-    <PostModal
-      caption='hi'
-      location='here'
-      createdAt={new Date('Sat Jan 02 2021')}
-      show={true}
-      fileString='string'
-      userName='Giuliano'
-      onHide={() => {}}
-      onOptionsClick={() => {}}
-      userProfilePhotoFile='userFile'
-      postId=''
-      userId=''
-      createPostReactionStart={(reactionReq) =>
-        createPostReactionStart(reactionReq)
-      }
-      postReactionsArray={[]}
-      postReactionConfirm={null}
-      postReactionError={null}
-      getPostReactionsStart={(postId) => getPostReactionsStart(postId)}
-      getPostReactionsConfirm=''
-      getPostReactionsError={null}
-      currentUser={null}
-      clearLocalState={false}
-      postReactingUsers={[]}
-      reactorPhotoFileArray={[]}
-      usersProfilePhotoConfirm={null}
-      deleteReactionConfirm={null}
-      deleteReactionError={null}
-      showPostEditForm={false}
-      editPostDetailsConfirm={null}
-      getPostFileStart={(fileReq) => getPostFileStart(fileReq)}
-      getOtherUserStart={(otherUserReq) => getOtherUserStart(otherUserReq)}
-      deleteReactionStart={(deleteReq) => deleteReactionStart(deleteReq)}
-      setPostLikingUsersArray={(postLikingUsersArray) =>
-        setPostLikingUsersArray(postLikingUsersArray)
-      }
-      clearPostReactions={() => clearPostReactions()}
-      setShowPostEditForm={(show) => setShowPostEditForm(show)}
-      getSinglePostDataStart={(singlePostDataReq) =>
-        getSinglePostDataStart(singlePostDataReq)
-      }
-      clearPostState={() => clearPostState()}
-    />
-  );
+    return {
+      handleHide,
+      handleOptionsClick,
+      createPostReactionStart,
+      getPostReactionsStart,
+      getPostFileStart,
+      deleteReactionStart,
+      getOtherUserStart,
+      setPostLikingUsersArray,
+      clearPostReactions,
+      setShowPostEditForm,
+      getSinglePostDataStart,
+      clearPostState,
+    };
+  };
 
-  expect(postModal).toBeInTheDocument();
+  it('expect to render a post modal component', () => {
+    setup();
+
+    screen.debug();
+
+    // expect(postModal).toBeInTheDocument();
+  });
 });
