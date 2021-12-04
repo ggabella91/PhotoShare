41636763ade424807dc9235cfcd73c45bfc44be3
Giuliano Gabella <ggabella91@gmail.com>
2021-12-03 23:54:12 -0600

    Client: Additional change to post-options-modal component test suite, refactor of post-tile component test suite and implementation of two tests within it

--- client/src/components/__test__/post-tile.test.tsx
+++ client/src/components/__test__/post-tile.test.tsx
@@ -1,15 +1,37 @@
-import { render } from '../../test-utils/test-utils';
+import { render, screen, userEvent } from '../../test-utils/test-utils';
 import PostTile from '../post-tile/post-tile.component';
 
-it('expect to render a PostTile component', () => {
-  const { container: postTile } = render(
-    <PostTile
-      fileString='hello'
-      custRef={null}
-      dataS3Key=''
-      onClick={() => console.log('I was clicked!')}
-    />
-  );
+describe('expect to render a PostTile component', () => {
+  const setup = () => {
+    const handleClick = jest.fn();
 
-  expect(postTile).toBeInTheDocument();
+    render(
+      <PostTile
+        fileString='hello'
+        custRef={null}
+        dataS3Key='test-s3-key'
+        onClick={handleClick}
+      />
+    );
+
+    return { handleClick };
+  };
+
+  it('renders post tile component', () => {
+    setup();
+
+    const postTile = screen.getByRole('img');
+
+    expect(postTile).toBeInTheDocument();
+  });
+
+  it('clicking the post tile image calls the click handler function', () => {
+    const { handleClick } = setup();
+
+    const postTile = screen.getByRole('img');
+
+    userEvent.click(postTile);
+
+    expect(handleClick).toBeCalled();
+  });
 });
