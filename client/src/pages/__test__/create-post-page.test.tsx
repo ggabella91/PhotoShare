16f6f2ac36c135e77fbcea6dd06adc6e48f34e87
Giuliano Gabella <ggabella91@gmail.com>
2021-12-17 02:02:00 +0000

    Client: Added another test to the create-post-page component test suite

--- client/src/pages/__test__/create-post-page.test.tsx
+++ client/src/pages/__test__/create-post-page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '../../test-utils/test-utils';
+import { render, screen, userEvent } from '../../test-utils/test-utils';
 import { CreatePostPage } from '../create-post-page/create-post-page.component';
 
 import { User } from '../../redux/user/user.types';
@@ -10,6 +10,7 @@ describe('create-post page component tests', () => {
     const createPostStart = jest.fn();
     const clearPostStatuses = jest.fn();
     const getUsersFollowingStart = jest.fn();
+    global.URL.createObjectURL = jest.fn();
 
     render(
       <CreatePostPage
@@ -25,11 +26,56 @@ describe('create-post page component tests', () => {
     return { createPostStart, clearPostStatuses, getUsersFollowingStart };
   };
 
+  const isFile = (
+    testObj: FormDataEntryValue | File | null
+  ): testObj is File => {
+    return (testObj as File).name !== undefined;
+  };
+
   it('renders a create-post page component', () => {
     setup();
 
     const createPostPage = screen.getByText('Create a New Post');
 
     expect(createPostPage).toBeInTheDocument();
+  });
+
+  it("uploading a post image file and clicking 'Upload photo' calls create post start action creator", () => {
+    const { createPostStart } = setup();
+
+    const testPostFile = new File(['test-post-file'], 'test-post', {
+      type: 'img/jpeg',
+    });
+    const testCaption = 'This is my test post boiii';
+    const testLocation = 'That one chill place';
+
+    const postFileInput: HTMLInputElement =
+      screen.getByLabelText('Select photo');
+    const captionInput = screen.getByLabelText('Add a caption');
+    const locationInput = screen.getByLabelText('Where was this taken?');
+    const uploadButton = screen.getByText('Upload photo');
+
+    userEvent.upload(postFileInput, testPostFile);
+    userEvent.type(captionInput, testCaption);
+    userEvent.type(locationInput, testLocation);
+    userEvent.click(uploadButton);
+
+    const testFormData = new FormData();
+    testFormData.append('photo', testPostFile, testPostFile.name);
+    testFormData.append('caption', testCaption);
+    testFormData.append('location', testLocation);
+
+    const mockArgs = createPostStart.mock.calls[0][0];
+
+    if (postFileInput.files && isFile(postFileInput.files[0])) {
+      expect(postFileInput.files[0]).toStrictEqual(testPostFile);
+    }
+    expect(createPostStart).toBeCalled();
+    if (isFile(testFormData.get('photo'))) {
+      expect(mockArgs.get('photo').name).toEqual(
+        (testFormData.get('photo') as File).name
+      );
+    }
+    expect(global.URL.createObjectURL).toBeCalled();
   });
 });
