512e347b5414b38f4d121240819f0dc7053aa943
Giuliano Gabella <ggabella91@gmail.com>
2021-12-27 19:43:07 -0800

    Client: Created stub component for explore-tag-page, added route to the new page in the main App component

---
+++ client/src/pages/explore-tag-page/explore-tag-page.component.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+import './explore-tag-page.styles.scss';
+
+interface ExploreTagPageProps {
+  hashtag: string;
+}
+
+const ExploreTagPage: React.FC<ExploreTagPageProps> = ({ hashtag }) => {
+  return <div></div>;
+};
+
+export default ExploreTagPage;
