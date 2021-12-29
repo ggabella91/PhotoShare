f59566ef6abb483c8ba43e157023d97ac7cc3d50
Giuliano Gabella <ggabella91@gmail.com>
2021-12-28 17:23:47 -0800

    Posts: Updated get-posts-with-hashtag route handler to use pagination

--- posts/src/routes/get-posts-with-hashtag.ts
+++ posts/src/routes/get-posts-with-hashtag.ts
@@ -14,16 +14,58 @@ router.get(
   currentUser,
   async (req: Request, res: Response) => {
     const { hashtag } = req.params;
+    const pageToShow = parseInt(req.query.pageToShow as string) || null;
+    const limit = parseInt(req.query.limit as string) || null;
 
     if (!hashtag) {
       throw new BadRequestError('No hashtag was provided.');
     }
 
-    let postsWithHashtag = await Post.find({
-      hashtags: hashtag,
-    });
+    let postsWithHashtag;
 
-    res.status(200).send(postsWithHashtag);
+    if (pageToShow && limit) {
+      let queryLength = 0;
+
+      if (pageToShow === 1) {
+        queryLength = (
+          await Post.find(
+            { hashtags: hashtag, archived: { $ne: true } },
+            null,
+            {
+              sort: { _id: -1 },
+            }
+          )
+        ).length;
+      }
+
+      postsWithHashtag = await Post.find(
+        { hashtags: hashtag, archived: { $ne: true } },
+        null,
+        {
+          sort: { _id: -1 },
+        }
+      )
+        .limit(limit)
+        .skip((pageToShow - 1) * limit);
+      console.log(`Data for posts with hashtag: ${hashtag}`, postsWithHashtag);
+
+      if (queryLength) {
+        res.status(200).send({ postsWithHashtag, queryLength });
+      } else {
+        res.status(200).send({ postsWithHashtag });
+      }
+    } else {
+      postsWithHashtag = await Post.find(
+        { hashtags: hashtag, archived: { $ne: true } },
+        null,
+        {
+          sort: { _id: -1 },
+        }
+      );
+      console.log(`Data for posts with hashtag: ${hashtag}`, postsWithHashtag);
+
+      res.status(200).send({ postsWithHashtag });
+    }
   }
 );
 
