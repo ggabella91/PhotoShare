c62d16b5e81d954697f5cbc312dc7d7f86b13ada
Giuliano Gabella <ggabella91@gmail.com>
2022-07-20 22:03:45 -0500

    Messages: Updated dtos for creating and updating conversations, along with conversation schema, to include conversation admins. Updated service methods for creating and updating conversation to set and check for admins in a conversation before making changes

--- messages/src/database/dto/update-convo-dto.ts
+++ messages/src/database/dto/update-convo-dto.ts
@@ -1,11 +1,13 @@
 import { User } from '../schemas/user.schema';
 
 export class UpdateConvoPreDto {
+  updatingUser: User;
   id: string;
   name?: string;
   avatarS3Keys?: string[];
   connectedUsers?: User[];
   connectedUserNames?: string[];
+  adminUsers: User[];
 }
 
 export class UpdateConvoDto {
@@ -13,4 +15,5 @@ export class UpdateConvoDto {
   avatarS3Keys?: string[];
   connectedUsers?: string[];
   connectedUserNames?: string[];
+  adminUsers?: string[];
 }
