93ea348cce6777e074cdec4b0e2ca3a13159848e
Giuliano Gabella <ggabella91@gmail.com>
2022-12-23 23:22:39 -0800

    Client: Add notifications container component skeleton, include it in header component

---
+++ client/src/components/header/notifications-container.component.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const NotificationsContainer: React.FC = () => {
+  return <Grid></Grid>;
+};
+
+export default NotificationsContainer;
