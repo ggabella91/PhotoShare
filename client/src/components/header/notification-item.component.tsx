9cc362dd4c88c50371f47341f7b670cc19dff223
Giuliano Gabella <ggabella91@gmail.com>
2023-01-06 18:12:08 -0600

    Client: Update notification-item component to use button that navigates user to associated post (when applicable)

--- client/src/components/header/notification-item.component.tsx
+++ client/src/components/header/notification-item.component.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Grid, Button, Typography } from '@mui/material';
 import { Notification } from '../../redux/follower/follower.types';
 
 interface NotificationItemProps {
   notification: Notification;
 }
 
-// TODO: Make component a button; add click handler that will,
-// if the notification involves a post, navigate user to that post
-
 const NotificationItem: React.FC<NotificationItemProps> = ({
   notification,
 }) => {
-  const { message, createdAt } = notification;
+  const { message, createdAt, postId } = notification;
+  const navigate = useNavigate();
+
+  const handleClickNotificationItem = () => {
+    if (postId) {
+      navigate(`/p/${postId}`);
+    }
+  };
 
   return (
     <Grid sx={{ display: 'flex', flexDirection: 'row', height: '60px' }}>
       <Grid>{/* Avatar will go here */}</Grid>
-      <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Button
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          minWidth: 'unset',
+          height: '100%',
+          textTransform: 'unset',
+          padding: 0,
+          '&:hover': {
+            backgroundColor: 'unset',
+          },
+        }}
+        disableRipple
+        onClick={handleClickNotificationItem}
+      >
         <Typography>{message}</Typography>
         <Typography>{createdAt.toDateString()}</Typography>
-      </Grid>
+      </Button>
     </Grid>
   );
 };
