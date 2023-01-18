b55c4768ebf5c316bbc8935e2b3080acf7dbf164
Giuliano Gabella <ggabella91@gmail.com>
2023-01-17 18:32:20 -0600

    Client: Update notifications-container component to ensure notification-item components are ready to render before actually rendering

--- client/src/components/header/notifications-container.component.tsx
+++ client/src/components/header/notifications-container.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Grid } from '@mui/material';
 import NotificationItem from './notification-item.component';
@@ -12,8 +12,10 @@ import { selectNotifications } from '../../redux/follower/follower.selectors';
 import { getNotificationsStart } from '../../redux/follower/follower.actions';
 import { getOtherUserStart } from '../../redux/user/user.actions';
 import { OtherUserType } from '../../redux/user/user.types';
+import { Notification } from '../../redux/follower/follower.types';
 
 const NotificationsContainer: React.FC = () => {
+  const [readyToRender, setReadyToRender] = useState(false);
   const currentUser = useSelector(selectCurrentUser);
   const notifications = useSelector(selectNotifications);
   const notificationUsers = useSelector(selectNotificationUsers);
@@ -33,12 +35,12 @@ const NotificationsContainer: React.FC = () => {
 
   useEffect(() => {
     if (notifications?.length) {
-      notifications.forEach((notification) => {
-        if (!(notification.fromUserId in notificationUsers)) {
+      notifications.forEach(({ fromUserId }) => {
+        if (!(notificationUsers && fromUserId in notificationUsers)) {
           dispatch(
             getOtherUserStart({
               type: OtherUserType.NOTIFICATION_USER,
-              usernameOrId: notification.fromUserId,
+              usernameOrId: fromUserId,
             })
           );
         }
@@ -46,15 +48,33 @@ const NotificationsContainer: React.FC = () => {
     }
   }, [dispatch, notifications, notificationUsers]);
 
+  console.log('notificationUsers: ', notificationUsers);
+
+  useEffect(() => {
+    if (notificationUsers) {
+      const ready = notifications?.every(
+        (notification: Notification) =>
+          notificationUsers?.[notification.fromUserId]
+      );
+
+      setReadyToRender(ready || false);
+    }
+  }, [notifications, notificationUsers]);
+
   return (
     <Grid sx={{ display: 'flex', flexDirection: 'column', width: 'inherit' }}>
-      {notifications?.map((notification) => (
-        <NotificationItem
-          notification={notification}
-          key={notification.id}
-          user={notificationUsers[notification.fromUserId]}
-        />
-      ))}
+      {readyToRender &&
+        notifications?.map((notification) => (
+          <NotificationItem
+            notification={notification}
+            key={notification.id}
+            user={
+              (notificationUsers?.[notification.fromUserId] &&
+                notificationUsers[notification.fromUserId]) ||
+              null
+            }
+          />
+        ))}
     </Grid>
   );
 };
