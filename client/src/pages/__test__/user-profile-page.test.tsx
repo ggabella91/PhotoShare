30ba7549527cefdaf1a4089e67e22c0f08c1588a
Giuliano Gabella <ggabella91@gmail.com>
2023-06-13 11:18:55 -0500

    Client: Remove user actions from mapDispatchToProps and update logic to dispatch those actions regularly in user-profile-page component; update corresponding unit test file

--- client/src/pages/__test__/user-profile-page.test.tsx
+++ client/src/pages/__test__/user-profile-page.test.tsx
@@ -3,26 +3,22 @@ import { UserProfilePage } from '../user-profile-page/user-profile-page.componen
 
 describe('user-profile-page component tests', () => {
   const setup = () => {
-    const getOtherUserStart = jest.fn();
     const getPostDataStart = jest.fn();
     const getPostFileStart = jest.fn();
     const followNewUserStart = jest.fn();
     const clearPostState = jest.fn();
     const getFollowersStart = jest.fn();
-    const clearFollowersAndFollowing = jest.fn();
     const clearFollowPhotoFileArray = jest.fn();
     const getUsersFollowingStart = jest.fn();
     const clearFollowState = jest.fn();
     const unfollowUserStart = jest.fn();
     const clearPostFilesAndData = jest.fn();
-    const setIsCurrentUserProfilePage = jest.fn();
     const setShowCommentOptionsModal = jest.fn();
     const deleteReactionStart = jest.fn();
 
     render(
       <UserProfilePage
         username='giuliano_gabella'
-        getOtherUserStart={getOtherUserStart}
         getPostDataStart={getPostDataStart}
         getPostFileStart={getPostFileStart}
         clearFollowPhotoFileArray={clearFollowPhotoFileArray}
@@ -30,10 +26,8 @@ describe('user-profile-page component tests', () => {
         getFollowersStart={getFollowersStart}
         getUsersFollowingStart={getUsersFollowingStart}
         unfollowUserStart={unfollowUserStart}
-        clearFollowersAndFollowing={clearFollowersAndFollowing}
         clearPostFilesAndData={clearPostFilesAndData}
         clearFollowState={clearFollowState}
-        setIsCurrentUserProfilePage={setIsCurrentUserProfilePage}
         setShowCommentOptionsModal={setShowCommentOptionsModal}
         deleteReactionStart={deleteReactionStart}
         clearPostState={clearPostState}
@@ -41,36 +35,28 @@ describe('user-profile-page component tests', () => {
     );
 
     return {
-      getOtherUserStart,
       getPostDataStart,
       getPostFileStart,
       followNewUserStart,
       clearPostState,
       getFollowersStart,
-      clearFollowersAndFollowing,
       clearFollowPhotoFileArray,
       getUsersFollowingStart,
       clearFollowState,
       unfollowUserStart,
       clearPostFilesAndData,
-      setIsCurrentUserProfilePage,
       setShowCommentOptionsModal,
       deleteReactionStart,
     };
   };
 
   it('renders a user-profile-page component', () => {
-    const {
-      getOtherUserStart,
-      getPostFileStart,
-      getFollowersStart,
-      getUsersFollowingStart,
-    } = setup();
+    const { getPostFileStart, getFollowersStart, getUsersFollowingStart } =
+      setup();
 
     const userProfilePage = screen.getByTestId('user-profile-page');
 
     expect(userProfilePage).toBeInTheDocument();
-    expect(getOtherUserStart).toBeCalled();
     expect(getPostFileStart).toBeCalled();
     expect(getFollowersStart).toBeCalled();
     expect(getUsersFollowingStart).toBeCalled();
